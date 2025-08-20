8581abf499a5ad6a4d7e8acf50ad8c15f2fac585
SakoA10 <sakoa04@hotmail.com>
2025-08-20 10:15:19 +0300

    Update

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ balls = []
 
 max_lanew=200
 
-colors = [color.red, color.green, color.blue]
+colors = [color.red,color.green,color.blue,color.cyan,color.magenta,color.yellow,color.white,color.black,color.orange,color.purple]
 
-number_of_balls = random.randint(100, 150)
+
+number_of_balls = random.randint(50,70)
 
 for i in range(number_of_balls):
   ball = sphere (pos=vec(random.randint((i*max_lanew+3),(i*max_lanew+max_lanew-3))-(number_of_balls*(max_lanew/2)), random.randint(100,110), 0), radius=random.randint(25,30), color=random.choice(colors))
@@ -20,6 +21,3 @@ while True:
   time=time+1
   for ball in balls:
     ball.pos.y=ball.pos.y-time*5
-    
-
-print("sss")
